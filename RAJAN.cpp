38edbfc7baa88d7810d872dcfb252679742509b3
Rajan Deo <viperson90@gmail.com>
2026-02-06 14:31:42 -0500

    Add files via upload

---
+++ RAJAN.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include "cachesim.hpp"
+#include <stdio.h>
+#include <inttypes.h>
+#include <stdlib.h>
+#include <string.h>
+
+using namespace std;
+
+int numOffsetBits;
+int numIndexBits;
+int numTagBits;
+int main() {
+
+    sim_config_t config = DEFAULT_SIM_CONFIG;
+    
+
+    sim_setup(&config);
+
+    return 0;
+}    
+
+
+void sim_setup(sim_config_t *config) {
+
+    numOffsetBits = config->l1_config.b;
+    numIndexBits = config->l1_config.c - config->l1_config.b - config->l1_config.s;
+    numTagBits = 64 - numOffsetBits - numIndexBits;
+
+    int numOfSets = (1) << numIndexBits; //2^#ofIndexBits
+    int numOfWays = (1) << config->l1_config.s; //2^s
+
+    uint64_t address = 0x0000560feb6d7f70;
+
+    uint64_t addressIndex = (address >> numOffsetBits) & ((1ULL << numIndexBits) - 1);
+    uint64_t addressTag = (address >> (numOffsetBits + numOffsetBits)) & ((1 << numTagBits) - 1);
+
+    cout << numOffsetBits << endl;
+    cout << numIndexBits << endl;
+    cout << numTagBits << endl;
+    cout << addressIndex << endl;
+    cout << addressTag << endl;
+    
+
+
+}
+
