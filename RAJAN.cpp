e2bfadbabd35a1b7c0e3c6cbd12430af542c9ab6
Rajan Deo <viperson90@gmail.com>
2026-02-07 12:06:41 -0500

    Add files via upload

--- RAJAN.cpp
+++ RAJAN.cpp
@@ -7,21 +7,33 @@
 
 using namespace std;
 
+sim_stats_t stats;
+
 int numOffsetBits;
 int numIndexBits;
 int numTagBits;
 int main() {
 
     sim_config_t config = DEFAULT_SIM_CONFIG;
+
+    
     
 
     sim_setup(&config);
 
-    return 0;
+    stats.hits_l1++;
+    stats.hits_l1++;
+    printf("%d",stats.hits_l1);
+
+
+    char test = 'W';
+
 }    
 
 
 void sim_setup(sim_config_t *config) {
+
+    memset(&stats, 0, sizeof(sim_stats_t));
 
     numOffsetBits = config->l1_config.b;
     numIndexBits = config->l1_config.c - config->l1_config.b - config->l1_config.s;
@@ -33,7 +45,7 @@ void sim_setup(sim_config_t *config) {
     uint64_t address = 0x0000560feb6d7f70;
 
     uint64_t addressIndex = (address >> numOffsetBits) & ((1ULL << numIndexBits) - 1);
-    uint64_t addressTag = (address >> (numOffsetBits + numOffsetBits)) & ((1 << numTagBits) - 1);
+    uint64_t addressTag = (address >> (numOffsetBits + numIndexBits)); //& ((1 << numTagBits) - 1);
 
     cout << numOffsetBits << endl;
     cout << numIndexBits << endl;
--- cachesim.cpp
+++ cachesim.cpp
@@ -7,9 +7,13 @@
  * TODO: You're responsible for completing this routine
  */
 
+//sim_stats_t stats;
+    
 int numOffsetBits;
 int numIndexBits;
 int numTagBits;
+
+uint64_t cacheTimer; //instead of dealing with data structure for LRU, each block just holds last time it was accessed? this will then determine which one was accessed last.
 
 int numOfWays;
 int numOfSets;
@@ -35,10 +39,11 @@ Block **cache;
 
 //The L1 cache uses a write-back, write-allocate (WBWA) policy and uses MIP as its insertion policy
 
-
 using namespace std;
 
 void sim_setup(sim_config_t *config) {
+    cacheTimer = 0;
+    //memset(&stats, 0, sizeof(sim_stats_t));
 
     numOffsetBits = config->l1_config.b;
 
@@ -62,7 +67,7 @@ void sim_setup(sim_config_t *config) {
         
         //[numOfSets(0->7)][numOfWays(0->2)]
         for (int j = 0; j < numOfWays; j++) {
-            cache[i][j].valid = false;
+            cache[i][j].valid = 0;
             cache[i][j].tag = 0;
             cache[i][j].lruPosition = 0;
         }
@@ -75,19 +80,76 @@ void sim_setup(sim_config_t *config) {
  * TODO: You're responsible for completing this routine
  */
 void sim_access(char rw, uint64_t addr, sim_stats_t* stats) {
+    cacheTimer++;
 
     uint64_t addressIndex = (addr >> numOffsetBits) & ((1ULL << numIndexBits) - 1);
-    uint64_t addressTag = (addr >> (numOffsetBits + numOffsetBits)) & ((1 << numTagBits) - 1);
+    uint64_t addressTag = (addr >> (numOffsetBits + numIndexBits)); //& ((1 << numTagBits) - 1);
 
-    
+    stats->accesses_l1++;
 
 
     //for searching through all ways in a set
     for(int i = 0; i < numOfWays; i++) {
         if(cache[addressIndex][i].tag == addressTag && cache[addressIndex][i].valid) {
             //handle cache hit
+            cache[addressIndex][i].lruPosition = cacheTimer;
+            stats->hits_l1++;
+            if (rw == WRITE) {
+                cache[addressIndex][i].dirty = 1;
+                stats->writes++;
+            } else {
+                stats->reads++;
+            }
+            return;
+        } 
+    }
+
+    //if we searched and couldn't find a valid and matching tag, consider the fact we missed:
+    stats->misses_l1++;
+    //Check invalid bits first? maybe cold start cache and cache is filled with garbage that can be written over
+    for(int i = 0; i < numOfWays; i++) {
+        if(cache[addressIndex][i].valid != 1) {
+            cache[addressIndex][i].tag = addressTag;
+            cache[addressIndex][i].valid = 1;
+            cache[addressIndex][i].lruPosition = cacheTimer;
+            if (rw == WRITE) {
+                stats->writes++;
+                cache[addressIndex][i].dirty = 1;
+            } else {
+                stats->reads++;
+                cache[addressIndex][i].dirty = 0;
+            }
+            return;
         }
     }
+
+    //if that wasn't the case, then we have a full set, and must evict a block based on LRU, and insert using MRU insertion policy
+    uint64_t minTime = UINT64_MAX;
+    int wayToRemove = -1;
+    for(int i = 0; i < numOfWays; i++) {
+        if(cache[addressIndex][i].lruPosition < minTime) {
+            minTime = cache[addressIndex][i].lruPosition;
+            wayToRemove = i;
+        }
+    }
+
+    if(cache[addressIndex][wayToRemove].dirty == 1) {
+        stats->write_backs_l1++;
+        //handle write to l2 here later possibly
+    }
+
+    if (rw == WRITE) {
+        stats->writes++;
+    } else {
+        stats->reads++;
+    }
+
+
+    cache[addressIndex][wayToRemove].tag = addressTag;
+    cache[addressIndex][wayToRemove].valid = 1;
+    cache[addressIndex][wayToRemove].lruPosition = cacheTimer;
+    cache[addressIndex][wayToRemove].dirty = rw == WRITE ? 1 : 0;
+    
 
 }
 
@@ -97,5 +159,11 @@ void sim_access(char rw, uint64_t addr, sim_stats_t* stats) {
  * TODO: You're responsible for completing this routine
  */
 void sim_finish(sim_stats_t *stats) {
+
+    stats->hit_ratio_l1 = (double) (stats->hits_l1) / (double) (stats->accesses_l1);
+    stats->miss_ratio_l1 = (double) (stats->misses_l1) / (double) (stats->accesses_l1);
+
+    //AAT = HT + (MR * MP)
+    
 
 }
